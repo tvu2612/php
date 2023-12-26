e583b00e65c931563d307f49beb6a512c9970bce
tvu2612 <tvu16119@gmail.com>
2023-12-26 10:03:03 +0700

    up

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+    $int_var = 12345;
+    $another_int = -12345 + 12345;
+    $many = 2.123456;
+    $many_2 = 3.123456;
+    $few = $many + $many_2;
+
+    $array_1[1000] = "This is a array for demo";
+    $my_var = NULL; //null: ko xác định
+    $string_1 = "This is a string in php, do dai cua string nay la tuy y";
+    
+
+    if(TRUE){
+        print("This is a message");
+        echo"message";
+    }else{
+
+    }
+?>
